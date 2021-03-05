3ff48447159455107e2a5d03b91b148423de6e43
Juraj Hynek <juraj.hynek@gmail.com>
2021-03-05 06:09:46 +0000

    added app state func

---
+++ App/demo-apps/demo.appstate.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { Alert, View, Text, StyleSheet, AppState } from 'react-native';
+
+const style = StyleSheet.create({
+    container: {
+        justifyContent: 'center',
+        flex: 1
+    },
+    button: {
+        height: 70,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#ededed'
+    }
+})
+
+export default class RNAppState extends React.Component<{}, {}> {
+    state = {
+        appState: 'active'
+    }
+    componentDidMount() {
+        AppState.addEventListener('change', this.handleAppState)
+    }
+    handleAppState = (currentAppState: string) => {
+        this.setState(() => ({
+            appState: currentAppState
+        }));
+    }
+    public render() {
+
+        return <View style={[style.container]}>
+            <Text>AppState will tell you whether the app is active, inactive of in the background</Text>
+            <Text>AppState:  {this.state.appState} </Text>
+        </View>
+    }
+}
